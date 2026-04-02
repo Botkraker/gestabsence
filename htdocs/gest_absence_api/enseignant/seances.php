690ee9dbb38720c18e94166b94d4291022c81242
yacin <yassinebensassi321@gmail.com>
2026-04-02 21:13:46 +0100

    enseignement is added

--- htdocs/gest_absence_api/enseignant/seances.php
+++ htdocs/gest_absence_api/enseignant/seances.php
@@ -13,12 +13,26 @@ include_once '../config/database.php';
 $data = json_decode(file_get_contents("php://input"));
 //input here (seanceid:1,listabsence:[1,"present"])
 $response = array();
-
-if (!isset($_GET["id"])){
-    $
-}else{
-
+$sql="SELECT * FROM `seances`";
+if (isset($_GET["id"])){
+    $id=$_GET["id"];
+    $sql=$sql."WHERE id=$id";
 }
-
+$result= $db->query($sql.";");
+if ($result->num_rows>0){
+    while ($row = $result->fetch_assoc()) {
+        $seances_arr[] = $row;
+    }
+    http_response_code(200);
+    $response["success"]=1;
+    $response["message"]="Fetch Done";
+    $response["data"]=$seances_arr;
+    echo json_encode($response);
+}else{
+    http_response_code(404);
+    $response["success"]=0;
+    $response["message"]="Seance Not Found";
+    echo json_encode($response);
+}
 
 ?>
